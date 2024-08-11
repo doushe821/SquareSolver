9ead73a9a212c72011b0c9ff393b7ed2330eb4c5
doushe821 <romachevskii.ae@phystech.edu>
2024-08-11 18:22:50 +0300

    Добавил проверку ввода, инициализировал переменные, убрал второй вызов sqrt().

--- quad_eq.c
+++ quad_eq.c
@@ -1,48 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define NAME_LENGTH 81
 void solve_direct ();
+double get_input(char k);
 int main (void)
 {
-    char ch;
+    char ch = '0';
     puts("This application solves quadratic equations.");
     puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
     while ((ch = getchar()) != EOF)
     {
-    if (ch == 's')
-    {
-        solve_direct();
-        puts("Type \"s\" to continue solving equations, type \"q\" to exit program.");
-    }
-    else if (ch == 'q')
-    {
-        printf("Bye.");
-        exit(0);
-    }
+        if (ch == '\n')
+        {
+            puts("Invalid input.");
+            puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
+            continue;
+        }
+        if (ch == 's' && getc(stdin) == '\n')
+        {
+            solve_direct();
+            puts("Type \"s\" to continue solving equations, type \"q\" to exit program.");
+        }
+        else if (ch == 'q' && getc(stdin) == '\n')
+        {
+            printf("Bye.");
+            exit(0);
+        }
+        else
+        {
+            puts("Invalid input.");
+            puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
+            while(getchar() != '\n')
+                continue;
+        }
     }
 }
 void solve_direct ()
 {
-    while (getc(stdin) != '\n')
-        continue;
-    double a, b, c;
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    double d = 0;
     printf("Enter coefficients: ax^2 + bx + c = 0\n");
-    printf("a: ");
-    scanf("%lf", &a);
-    printf("b: ");
-    scanf("%lf", &b);
-    printf("c: ");
-    scanf("%lf", &c);
-    double d = b*b - 4*a*c;
+    a = get_input('a');
+    b = get_input('b');
+    c = get_input('c');
+    d = b*b - 4*a*c;
     if (d < 0)
         puts("Your equation has no real roots.");
     else if (d == 0)
-        printf("Your equation has single root: x = %.3lf\n", -b/(2*a));
-    else 
+        printf("Your equation has single root: x = %.12lf\n", -b/(2*a));
+    else
         {
+            d = sqrt(b*b - 4*a*c);
             printf("Your equation has two roots:\n");
-            printf("x_1 = %lf", (-b + sqrt(d))/(2*a));
-            printf("\tx_2 = %lf\n", (-b - sqrt(d))/(2*a));
+            printf("x_1 = %.12lf", (-b + d)/(2*a));
+            printf("\tx_2 = %.12lf\n", (-b - d)/(2*a));
         }
+}
+double get_input(char k)
+{
+    double v = 0;
+    char miss_input = '0';
+    while(1)
+    {
+        printf("%c: ", k);
+        if (scanf("%lf", &v) && getchar() == '\n')
+            break;
+        else
+        {
+            while((miss_input = getchar()) != '\n')
+                printf("%c", miss_input);
+            printf(" is not a number, try again.\n");
+        }    }
+    return(v);
 }
