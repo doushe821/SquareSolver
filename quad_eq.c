0d7575f6c6016d80c11f5744172f43d34300990f
doushe821 <romachevskii.ae@phystech.edu>
2024-08-13 21:52:11 +0300

    Исправил инициализацию переменных и структур, урбрал ненужные константы.
    Теперь переменные инициализируются перед использованием. Массивы и структуры инициализированы {0}.

--- quad_eq.c
+++ quad_eq.c
@@ -4,9 +4,6 @@
 #include <stdbool.h>
 
 const int inf_roots = -1;
-const int a = 0;
-const int b = 1;
-const int c = 2;
 
 struct user_input {
     double value;
@@ -26,15 +23,14 @@ struct user_input get_input (char k);
 
 int main (void)
 {
-    int i = 0;
     char coefficients_names[3] = {'a', 'b', 'c'};
-    double coefficients_values[3] = {0, 0, 0};
-    struct user_input input_check = {0, 0};
-    struct solve_output output = {0, 0, 0, 0};
-    char ch = 0;
+    double coefficients_values[3] = {0};
+    struct user_input input_check = {0};
+    struct solve_output output = {0};
 
     puts("This application solves quadratic equations.");
     puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
+    char ch = 0;
     while ((ch = getchar()) != EOF)
     {
         if (ch == '\n')
@@ -47,7 +43,7 @@ int main (void)
         {
             printf("Enter coefficients: ax^2 + bx + c = 0\n"
             "Press \"Ctrl+Z\" to return to menu.\n");
-            for (i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 input_check = get_input(coefficients_names[i]);
                 if (input_check.eof_flag)
@@ -59,7 +55,7 @@ int main (void)
                 else
                 coefficients_values[i] = input_check.value;
             }
-            output = solve_quad(coefficients_values[a], coefficients_values[b], coefficients_values[c]);
+            output = solve_quad(coefficients_values[0], coefficients_values[1], coefficients_values[2]);
             if (output.roots_num == 2)
                 printf("Equation has two roots:\n"
                 "x = %lf \t x = %lf\n", output.x1, output.x2);
@@ -96,7 +92,7 @@ int main (void)
 
 struct solve_output solve_quad (double a_s, double b_s, double c_s)
 {
-    struct solve_output solve_quad_out = {0, 0, 0, 0};
+    struct solve_output solve_quad_out = {0};
     double d = b_s*b_s - 4*a_s*c_s;
 
     if (a_s == 0 && b_s != 0)
@@ -118,7 +114,7 @@ struct solve_output solve_quad (double a_s, double b_s, double c_s)
     }
     else
     {
-        d = sqrt(b_s*b_s - 4*a_s*c_s);
+        d = sqrt(d);
         solve_quad_out.x1 = (-b_s + d)/(2*a_s);
         solve_quad_out.x2 = (-b_s - d)/(2*a_s);
         solve_quad_out.roots_num = 2;
@@ -129,12 +125,11 @@ struct solve_output solve_quad (double a_s, double b_s, double c_s)
 struct user_input get_input(char k)
 {
     struct user_input input = {0, 0};
-    char miss_input = 0;
-    int check = 0;
 
     while (1)
     {
         printf("%c: ", k);
+        int check = 0;
         if ((check = scanf("%lf", &input.value)) == 1 && getchar() == '\n')
             break;
         else if (check == -1)
@@ -144,6 +139,7 @@ struct user_input get_input(char k)
         }
         else
         {
+            char miss_input = 0;
             while ((miss_input = getchar()) != '\n')
                 printf("%c", miss_input);
             printf(" is not a number, try again.\n");
