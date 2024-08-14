1b89c0f3663cba4c562ca9d785b24c94f1024d73
doushe821 <romachevskii.ae@phystech.edu>
2024-08-14 16:42:30 +0300

    Ввод и вывод теперь совершаются в отдельных функциях.
    Удалил структуру user_input за ненадобностью (get_input теперь прнимает указатель на массив, в который передает значения коэффициентов, возвращает флаг на EOF во время ввода коэффициентов). Вместо повторяющейся строки из меню теперь строковая константа. В main появилась проверка на EOF. doublecmp: difference -> delta. Честно говоря, не понял, как можно одним выражением заменить последние 4 строки этой функции.

--- quad_eq.cpp
+++ quad_eq.cpp
@@ -3,14 +3,10 @@
 #include <math.h>
 #include <stdbool.h>
 #include <limits.h>
+#define MENU_INPUT "Type \"s\" to start solving equations, type \"q\" to exit program.\n"
 
 const double EPS = 1E-12;
 const int INF_ROOTS = -1;
-
-struct user_input {
-    double value;
-    bool eof_flag;
-};
 
 struct solve_output{
     double x1;
@@ -21,76 +17,57 @@ struct solve_output{
 
 struct solve_output solve_quad (double a_s, double b_s, double c_s);
 
-struct user_input get_input (char k);
+bool get_input (double * cfs);
 
-int doublecmp(double a, double b);
+int doublecmp(double a, double b, double acc);
+
+void answers_output(const struct solve_output console_out);
 
 int main (void)
 {
-    char coefficients_names[3] = {'a', 'b', 'c'};
     double coefficients_values[3] = {};
-    struct user_input input_check = {};
     struct solve_output output = {};
     puts("This application solves quadratic equations.");
-    puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
+    printf("%s", MENU_INPUT);
     int ch = 0;
-    while ((ch = getchar()) != EOF)
+    while ((ch = getchar()) != 'q')
     {
         if (ch == '\n')
         {
-
             puts("Invalid input.");
-            puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
+            printf("%s", MENU_INPUT);
             continue;
         }
         if (ch == 's' && getc(stdin) == '\n')
         {
-            printf("Enter coefficients: ax^2 + bx + c = 0\n"
-            "Press \"Ctrl+Z\" to return to menu.\n");
-            for (int i = 0; i < 3; i++)
+            printf("Enter coefficients: ax^2 + bx + c = 0\n");
+            bool eof_flag = get_input(coefficients_values);
+            if (eof_flag)
             {
-                input_check = get_input(coefficients_names[i]);
-                if (input_check.eof_flag)
-                {
-                    printf("Returning to menu.\n");
-                    break;
-                }
-                else
-                coefficients_values[i] = input_check.value;
+                printf("%s", MENU_INPUT);
+                continue;
             }
             output = solve_quad(coefficients_values[0], coefficients_values[1], coefficients_values[2]);
-            if (output.roots_num == 2)
-                printf("Equation has two roots:\n"
-                "x = %lf \t x = %lf\n", output.x1, output.x2);
-            else if (output.roots_num == 1)
-            {
-                if (output.is_linear)
-                    printf("Equation is linear and has one root: x = %lf\n", output.x1);
-                else
-                    printf("Equation has single root: x = %lf\n", output.x1);
-            }
-            else if (output.roots_num == INF_ROOTS)
-                printf("Equation has infinite amount of roots.\n");
-            else if (!output.roots_num)
-            {
-                printf("Equation has no roots.\n");
-            }
-
-            puts("Type \"s\" to continue solving equations, type \"q\" to exit program.");
+            answers_output(output);
+            printf("%s", MENU_INPUT);
         }
-        else if (ch == 'q' && getc(stdin) == '\n')
+        else if (ch == EOF)
         {
-            printf("Bye.");
-            exit(0);
+
+            puts("Invalid input.");
+            printf("%s", MENU_INPUT);
+                continue;
         }
         else
         {
             puts("Invalid input.");
-            puts("Type \"s\" to start solving equations, type \"q\" to exit program.");
+            printf("%s", MENU_INPUT);
             while (getchar() != '\n')
                 continue;
         }
     }
+    printf("Bye.");
+    return 0;
 }
 
 struct solve_output solve_quad (double a_s, double b_s, double c_s)
@@ -98,19 +75,19 @@ struct solve_output solve_quad (double a_s, double b_s, double c_s)
     struct solve_output solve_quad_out = {};
     double d = b_s*b_s - 4*a_s*c_s;
 
-    if (!doublecmp(a_s, 0) && doublecmp(b_s, 0))
+    if (!doublecmp(a_s, 0, EPS) && doublecmp(b_s, 0, EPS))
     {
         solve_quad_out.x1 = -c_s/b_s;
         solve_quad_out.roots_num = 1;
         solve_quad_out.is_linear = 1;
     }
-    else if (!doublecmp(a_s, 0) && !doublecmp(b_s, 0) && doublecmp(c_s, 0))
+    else if (!doublecmp(a_s, 0, EPS) && !doublecmp(b_s, 0, EPS) && doublecmp(c_s, 0, EPS))
         solve_quad_out.roots_num = 0;
-    else if (!doublecmp(a_s, 0) && !doublecmp(b_s, 0) && !doublecmp(c_s, 0))
+    else if (!doublecmp(a_s, 0, EPS) && !doublecmp(b_s, 0, EPS) && !doublecmp(c_s, 0, EPS))
         solve_quad_out.roots_num = INF_ROOTS;
-    else if (doublecmp(d, 0) == -1)
+    else if (doublecmp(d, 0, EPS) == -1)
         solve_quad_out.roots_num = 0;
-    else if (!doublecmp(d, 0))
+    else if (!doublecmp(d, 0, EPS))
     {
         solve_quad_out.x1 = -b_s/(2*a_s);
         solve_quad_out.roots_num = 1;
@@ -125,38 +102,60 @@ struct solve_output solve_quad (double a_s, double b_s, double c_s)
     return solve_quad_out;
 }
 
-struct user_input get_input(char k)
+bool get_input(double * cfs)
 {
-    struct user_input input = {};
-    while (1)
+    char coefficients_names[3] = {'a', 'b', 'c'};
+    for (int i = 0; i < 3; i++)
     {
-        printf("%c: ", k);
-        int check = 0;
-        if ((check = scanf("%lf", &input.value)) == 1 && getchar() == '\n')
-            break;
-        else if (check == -1)
+        while (1)
         {
-            input.eof_flag = 1;
-            break;
-        }
-        else
-        {
-            int miss_input = 0;
-            while ((miss_input = getchar()) != '\n')
-                printf("%c", miss_input);
-            printf(" is not a number, try again.\n");
+            printf("%c: ", coefficients_names[i]);
+            int check = 0;
+            if ((check = scanf("%lf", &cfs[i])) == 1 && getchar() == '\n')
+                break;
+            else if (check == -1)
+            {
+                return 1;
+            }
+            else
+            {
+                int miss_input = 0;
+                while ((miss_input = getchar()) != '\n')
+                    printf("%c", miss_input);
+                printf(" is not a number, try again.\n");
+            }
         }
     }
-    return input;
+    return 0;
 }
 
-int doublecmp(double a, double b)
+int doublecmp(double a, double b, double acc)
 {
-    double difference = a - b;
-    if (fabs(difference) <= EPS)
+    double delta = a - b;
+    if (fabs(delta) <= acc)
         return 0;
-    else if (difference >= EPS)
+    else if (delta > 0)
         return 1;
-    else if (difference <= -EPS)
+    else
         return -1;
+}
+
+void answers_output(const struct solve_output console_out)
+{
+    if (console_out.roots_num == 2)
+        printf("Equation has two roots:\n"
+        "x = %lf \t x = %lf\n", console_out.x1, console_out.x2);
+    else if (console_out.roots_num == 1)
+    {
+        if (console_out.is_linear)
+            printf("Equation is linear and has one root: x = %lf\n", console_out.x1);
+        else
+            printf("Equation has single root: x = %lf\n", console_out.x1);
+    }
+    else if (console_out.roots_num == INF_ROOTS )
+        printf("Equation has infinite amount of roots.\n");
+    else if (!console_out.roots_num)
+    {
+        printf("Equation has no roots.\n");
+    }
 }
